4239cb2bf675d13a8873bb82a5588f59e86975b3
microarij <49131703+microarij@users.noreply.github.com>
2019-05-06 01:12:39 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "en.h"
+int main()
+{ int x;
+x=equation();
+if (x==1) printf("sahhiiit");
+else printf("sorry ghaleeeet \n ");
+return 0 ;
+}
